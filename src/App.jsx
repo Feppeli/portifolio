cd5009a4d77da00bf27b258d64587393c34e724a
Douglas felipe <dfsilvat@gmail.com>
2024-02-14 11:33:08 -0300

    build: conquistas section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import perfilImage from './assets/Perfil image.png'
 
 import arrowIcon from './assets/icons/down-arrow.png'
 import trofeu from './assets/icons/trofeu.png'
+import premio1 from './assets/premio1.jpeg'
+import premio2 from './assets/premio2.png'
 
 function App() {
 
@@ -51,12 +53,41 @@ function App() {
       <section className="premiacoes">
         <div className='premiacoesHeader container'>
           <img src={trofeu} alt="trofeu" className='trofeuIcon' />
-          <h1>Premios</h1>
+          <h1>Conquistas</h1>
         </div>
 
         <div className="premiacoesContainer">
+
+
+
           <div className="premiacao">
-            
+            <div className="premiacaoImgContainer">
+
+
+              <img src={premio1} alt="" />
+
+              <div className="premiacaoTextContainer container">
+                <h3>The Dream Team</h3>
+                <p>O premio "The Dream Team" entregue pela Cubos Academy foi um grande marco para mim, tendo em vista a complexidade do projeto. </p>
+              </div>
+
+            </div>
+          </div>
+
+
+
+          <div className="premiacao">
+            <div className="premiacaoImgContainer">
+
+
+              <img src={premio2} alt="" />
+
+              <div className="premiacaoTextContainer container">
+                <h3>Aprovação - SISU 2024</h3>
+                <p>Algo inesperado aconteceu, enfrentei a prova desacreditado do resultado mas pronto para realizar o meu melhor. E o melhor aconteceu</p>
+              </div>
+
+            </div>
           </div>
         </div>
       </section>
